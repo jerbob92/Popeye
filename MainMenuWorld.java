ebe0635aed2b1e60d9ea14f2c3ed3f95272f3b4e
Jeroen Bobbeldijk <jeroen@devpros.nl>
2016-11-21 16:07:42 +0100

    Rename world, resize world

--- MainMenuWorld.java
+++ MainMenuWorld.java
@@ -1,21 +1,21 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * Write a description of class MainMenuWorld here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MyWorld extends World
+public class MainMenuWorld extends World
 {
 
     /**
      * Constructor for objects of class MyWorld.
      * 
      */
-    public MyWorld()
+    public MainMenuWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1280, 720, 1); 
     }
 }
