42f240309de19ac97c7f5d87c506c6b87cc24b68
Jeroen Bobbeldijk <jeroen@devpros.nl>
2016-12-12 15:24:54 +0100

    Start of minigame 2

---
+++ MiniGame2World.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MiniGame2World here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MiniGame2World extends World
+{
+
+    /**
+     * Constructor for objects of class MiniGame2World.
+     * 
+     */
+    public MiniGame2World()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1280, 720, 1, false); 
+    }
+}
